fdce98d20e15b4f56aa8628b2123811cdeba8907
ssuvj <125145157+ssuvj@users.noreply.github.com>
2026-07-25 23:55:32 +0300

    change2

--- android/infotaker.bash
+++ android/infotaker.bash
@@ -1,5 +1,5 @@
-# 1. needed termux
-# 2. needed shizuku (play store)
-# 3. needed canta debloater (play store)
-# 4. run shizuku and install rish in termux
+# <> needed termux
+# <> needed shizuku (play store)
+# <> needed canta debloater (play store)
+# <> run shizuku and install rish in tmux which runs in termux (pkg install tmux; tmux)
 
