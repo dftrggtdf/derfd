3ac38d4ae3e5eb739cc1c9bdff42ea84217d3abc
ssuvj <125145157+ssuvj@users.noreply.github.com>
2026-07-26 10:45:38 +0300

    change4

--- android/infotaker.bash
+++ android/infotaker.bash
@@ -5,5 +5,14 @@
 
 # 1. basic system information
 
-# 1.1 kernel version & build info
+# 1.1 kernel version & build info (tested)
 cat /proc/version
+
+# 1.2 android properties (tested)
+getprop | grep -E "ro.build|ro.product|ro.system|ro.vendor"
+
+# 1.3 android api level sdk (tested)
+getprop ro.build.version.sdk
+
+# 1.4 security patch level
+getprop ro.build.version.security_patch
