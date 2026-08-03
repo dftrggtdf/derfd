8c2e5f4f19ce1223ac046b34ffcb82d542ffd94c
dftrggtdf <125145157+dftrggtdf@users.noreply.github.com>
2026-08-03 20:32:40 +0300

    fahh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-ffmpeg discord convert: ffmpeg -i "input" -vcodec libx264 -crf 28 -acodec aac output.mp4
+my config files
+i am founder of node_project 
+some scripts i made in roblox games:
+    1. giant simulator auto rebirth before auto rebirthing
--- discord_ffmpeg.sh
+++ discord_ffmpeg.sh
@@ -1,9 +1,2 @@
-for file in *.mp4; do
-    if [ -f "$file" ]; then
-        size=$(stat -c%s "$file")
-        if [ "$size" -gt 25000000 ]; then
-            echo "Compressing $file for Discord..."
-            ffmpeg -y -i "$file" -vcodec libx264 -b:v 1500k -acodec aac -b:a 128k "discord_${file}"
-        fi
-    fi
-done
+# short video works better
+ffmpeg discord convert: ffmpeg -i "input" -vcodec libx264 -crf 28 -acodec aac output.mp4
